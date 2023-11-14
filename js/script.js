9603bf9d25ff675eb767ca852b07385ac3675177
naiara-ada <naiara.ada@gmail.com>
2023-11-14 10:47:40 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import peliculas from './peliculas.js'
-console.log(peliculas);
 
 const accion = peliculas.filter((elemento) => elemento.genre_ids.some((elemento) => elemento === 28));
 const thriller = peliculas.filter((elemento) => elemento.genre_ids.some((elemento) => elemento === 53));
@@ -9,26 +8,27 @@ const accionID = document.getElementById('genero-28');
 const thrillerID = document.getElementById('genero-53');
 const aventuraID = document.getElementById('genero-12');
 
+const posterURL ='https://image.tmdb.org/t/p/w500';
+
 accion.forEach((pelis) =>{
-    let poster = 'https://image.tmdb.org/t/p/w500' + pelis.poster_path;
+    let poster = posterURL + pelis.poster_path;
     let hija = document.createElement('div');
-    hija.innerHTML = `<img src=${poster} alt="poster" /> ${pelis.original_title}`
+    hija.innerHTML = `<img src=${poster} alt="poster" /> ${pelis.original_title}`;
     accionID.appendChild(hija);
 })
 
 thriller.forEach((pelis)=>{
-    let poster = 'https://image.tmdb.org/t/p/w500' + pelis.poster_path;
+    let poster = posterURL + pelis.poster_path;
     let hija = document.createElement('div');
-    hija.innerHTML = `<img src=${poster} alt="poster" /> ${pelis.original_title}`
+    hija.innerHTML = `<img src=${poster} alt="poster" /> ${pelis.original_title}`;
     thrillerID.appendChild(hija);       
 })
 
 aventura.forEach((pelis)=>{
-    let poster = 'https://image.tmdb.org/t/p/w500' + pelis.poster_path;
+    let poster = posterURL + pelis.poster_path;
     let hija = document.createElement('div');
-    hija.innerHTML = `<img src=${poster} alt="poster" /> ${pelis.original_title}`
+    hija.innerHTML = `<img src=${poster} alt="poster" /> ${pelis.original_title}`;
     aventuraID.appendChild(hija);       
 })
 
-console.log(aventuraID);
 
